c260a071f79756c5d56766b8f1df3dc30e7fdfe1
ernest nnodu <ernest.nnodu@outlook.com>
2025-04-29 11:27:03 +0100

    Create user model

---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/User.java
@@ -0,0 +1,12 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+}
