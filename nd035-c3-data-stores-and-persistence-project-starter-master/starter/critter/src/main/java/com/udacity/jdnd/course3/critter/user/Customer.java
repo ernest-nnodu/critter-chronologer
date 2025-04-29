1a643b94e19d259a25d7e48c09f25daf54e9750b
ernest nnodu <ernest.nnodu@outlook.com>
2025-04-29 11:33:37 +0100

    Create customer model

---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,16 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.PetDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Customer extends User{
+
+    private String phoneNumber;
+    private String notes;
+    private List<PetDTO> pets;
+}
