a1f4a5edc870cd22159396541fa40f42bb4663ad
ernest nnodu <ernest.nnodu@outlook.com>
2025-04-29 11:41:37 +0100

    Create pet model

---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,18 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Pet {
+
+    private long id;
+    private PetType type;
+    private String name;
+    private long ownerId;
+    private LocalDate birthDate;
+    private String notes;
+}
