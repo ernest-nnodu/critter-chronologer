e20a33650843b21d9b75c2ae31642d6ffb7ef3be
ernest nnodu <ernest.nnodu@outlook.com>
2025-05-04 22:20:38 +0100

    Refactor pet mapper

--- critter-chronologer/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetMapper.java
+++ critter-chronologer/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetMapper.java
@@ -1,6 +1,5 @@
 package com.udacity.jdnd.course3.critter.pet;
 
-import com.udacity.jdnd.course3.critter.user.Customer;
 import com.udacity.jdnd.course3.critter.user.CustomerRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
@@ -10,17 +9,13 @@ import org.springframework.stereotype.Component;
 public class PetMapper {
 
     private final ModelMapper mapper;
-    private final CustomerRepository customerRepository;
 
     public PetMapper(ModelMapper mapper, CustomerRepository customerRepository) {
         this.mapper = mapper;
-        this.customerRepository = customerRepository;
-
         configureMappings();
     }
 
     private void configureMappings() {
-        // Pet → PetDTO: map owner.id to ownerId
         mapper.addMappings(new PropertyMap<Pet, PetDTO>() {
             @Override
             protected void configure() {
@@ -28,7 +23,6 @@ public class PetMapper {
             }
         });
 
-        // PetDTO → Pet: skip owner, set manually
         mapper.addMappings(new PropertyMap<PetDTO, Pet>() {
             @Override
             protected void configure() {
